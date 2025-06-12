0420ae467faaf75081d441747837dd8870181efa
Nodge <76185358+KartWrightfield@users.noreply.github.com>
2025-06-12 12:56:21 +0100

    Keeping tests alive
    - Just an initial fix to match the DI change to the controller. From now on I will try and keep the test changes grouped with the relevant codebase changes.

--- UE.PostOffice.Tests/PostOfficeTests.cs
+++ UE.PostOffice.Tests/PostOfficeTests.cs
@@ -1,4 +1,6 @@
-﻿namespace UE.PostOffice.Tests
+﻿using UE.PostOffice.Data;
+
+namespace UE.PostOffice.Tests
 {
     using System;
     using System.Collections.Generic;
@@ -8,43 +10,46 @@
 
     public class PostOfficeTests
     {
+        private readonly DespatchDateController _controllerUnderTest;
+
+        public PostOfficeTests()
+        {
+            IDbContext dbContext = new DbContext();
+            _controllerUnderTest = new DespatchDateController(dbContext);
+        }
+        
         [Fact]
         public void OneProductWithLeadTimeOfOneDay()
         {
-            DespatchDateController controller = new DespatchDateController();
-            var date = controller.Get(new List<int>() {1}, DateTime.Now);
+            var date = _controllerUnderTest.Get(new List<int>() {1}, DateTime.Now);
             date.Date.Date.ShouldBe(DateTime.Now.Date.AddDays(1));
         }
 
         [Fact]
         public void OneProductWithLeadTimeOfTwoDay()
         {
-            DespatchDateController controller = new DespatchDateController();
-            var date = controller.Get(new List<int>() { 2 }, DateTime.Now);
+            var date = _controllerUnderTest.Get(new List<int>() { 2 }, DateTime.Now);
             date.Date.Date.ShouldBe(DateTime.Now.Date.AddDays(2));
         }
 
         [Fact]
         public void OneProductWithLeadTimeOfThreeDay()
         {
-            DespatchDateController controller = new DespatchDateController();
-            var date = controller.Get(new List<int>() { 3 }, DateTime.Now);
+            var date = _controllerUnderTest.Get(new List<int>() { 3 }, DateTime.Now);
             date.Date.Date.ShouldBe(DateTime.Now.Date.AddDays(3));
         }
 
         [Fact]
-        public void SaturdayHasExtraTwoDays()
-        {
-            DespatchDateController controller = new DespatchDateController();
-            var date = controller.Get(new List<int>() { 1 }, new DateTime(2018,1,26));
+        public void SaturdayHasExtraTwoDays() {
+
+            var date = _controllerUnderTest.Get(new List<int>() { 1 }, new DateTime(2018,1,26));
             date.Date.ShouldBe(new DateTime(2018, 1, 26).Date.AddDays(3));
         }
 
         [Fact]
         public void SundayHasExtraDay()
         {
-            DespatchDateController controller = new DespatchDateController();
-            var date = controller.Get(new List<int>() { 3 }, new DateTime(2018, 1, 25));
+            var date = _controllerUnderTest.Get(new List<int>() { 3 }, new DateTime(2018, 1, 25));
             date.Date.ShouldBe(new DateTime(2018, 1, 25).Date.AddDays(4));
         }
     }
